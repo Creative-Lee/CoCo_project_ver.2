f63040c8c8a04260fe7d0d50a3618311834198ec
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-05-20 21:44:55 +0900

    21.05.20
    교통사고 크리

---
+++ src/data.js
@@ -0,0 +1,41 @@
+
+let data = [
+    {
+        id : 0,
+        title : "White and Black",
+        content : "Born in France",
+        price : 120000
+    },
+
+    {
+        id : 1,
+        title : "Red Knit",
+        content : "Born in Seoul",
+        price : 110000
+    },
+
+    {
+        id : 2,
+        title : "Grey Yordan",
+        content : "Born in the States",
+        price : 130000
+    },
+    {
+        id : 3,
+        title : "Captin Coco",
+        content : "Born in mars",
+        price : 999999
+    }
+] 
+export default data;
+
+
+//내보내고 싶다면
+// let love = "true";
+// export default love;
+
+//내보낼 변수가 많다면
+// let name = "kim";
+// let name2 = "park";
+
+// export {name,name2};
