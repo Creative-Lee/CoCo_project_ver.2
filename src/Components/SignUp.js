4f2ac34d336ea691c4a6b8eabdc4ad6303634efc
Dohyeon Lee_Laptop <dlehgus1110@naver.com>
2022-04-22 21:10:56 +0900

    everyInputErrorList state생성 error 관련 로직 전부 수정 immutable.js 라이브러리 도입
    모든 input의 에러를 everyInputErrorList가 관리한다.
    모든 'XXXErrorHandler' 함수들은 개별로직에 따라 everyInputErrorList에서 자신의 에러 상태를 변경한다.
    모든 에러메세지는 everyInputErrorList에서 자신의 에러상태에 따라 나타나거나 사라진다.
    에러스타일은 everyInputErrorList에서 자신의 에러중 하나라도 true상태라면 적용된다. (이것만 미완성)

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import {Link,useNavigate} from 'react-router-dom'
+import { Map ,fromJS ,toJS, set, setIn, get, getIn} from 'immutable';
 
 import {Form, Button, Container, Row, Col} from 'react-bootstrap'
 
@@ -10,22 +11,57 @@ export default function SignUp({signUpEmail, jjongLogo2}){
   const [inputPassword, setInputPassword] = useState('') 
   const [inputPasswordCheck, setInputPasswordCheck] = useState('') 
 
-  const [isEmailInputEmpty, setIsEmailInputEmpty] = useState(false)
-  const [isPasswordInputEmpty, setIsPasswordInputEmpty] = useState(false)
-  const [isPasswordCheckInputEmpty, setIsPasswordCheckInputEmpty] = useState(false)
+  const [everyInputErrorList ,setEveryInputErrorList] = useState(fromJS({
+    localEmail : {
+      emptyError : false
+    },
+    domainEmail : {
+      emptyError : false,
+    },
+    password : {
+      emptyError : false,
+      shortError : false,
+    },
+    passwordCheck : {
+      emptyError : false,
+      notMatchError : false,
+    }
+  }))
 
-  const emptyErrorStyleController = (input, inputName, setStateFuncOfTarget) => {
-    const targetForm = document.getElementsByClassName(`form__${inputName}`)
+  const errorStyleHandler = (inputState, inputName) => {
+    // const eachInputErrorList = Object.values(everyInputErrorList.getIn([input]).toJS())
+    // const isIncludeError = eachInputErrorList.includes(true)
 
-    if(input){
-      setStateFuncOfTarget(false)
+    const targetForm = document.getElementsByClassName(inputName)
+
+    // console.log(targetForm)
+    // console.log(everyInputErrorList.getIn([input]).toJS())
+    // console.log(isIncludeError)  
+    // 원하는게 아닌데 ......
+    // 이건 인풋 엠티에만 한정됨
+    // 나는 어떤 에러든 true 상태로 있으면 에러스타일 적용되길 바람
+    // 근데 onBlur로 에러가상태가 true로 업데이트 되는 순간에는 true가 include가 아님...
+    // 고로 2번 onBlur해야 원하는대로 동작함
+    if(inputState) {
       targetForm[0].classList.remove('errored')
     }
-    else{
-      setStateFuncOfTarget(true)
+    else {
       targetForm[0].classList.add('errored')
     }
   }
+
+  const emptyErrorHandler = (inputState , inputName) => {
+    let changedList
+    if(inputState){
+      changedList = everyInputErrorList.setIn([inputName,'emptyError'], false) 
+      setEveryInputErrorList(changedList)
+    }
+    else{
+      changedList = everyInputErrorList.setIn([inputName,'emptyError'], true) 
+      setEveryInputErrorList(changedList) 
+    }
+  }
+
 
   const [checkedBoxIdList, setCheckedBoxIdList] = useState([])
   
@@ -64,7 +100,8 @@ export default function SignUp({signUpEmail, jjongLogo2}){
     { id: 3 , label: '이벤트, 프로모션 알림 메일 및 SMS 수신', span : '(선택)'}
   ]
 
-  return (    
+  return (  
+      
     <Container id="sign-up__container">
       <Link to='/CoCo_project_ver.2' >
         <div className='sign-up__logo-wrap'>
@@ -76,24 +113,31 @@ export default function SignUp({signUpEmail, jjongLogo2}){
       </div>
 
       <Form className="sign-up__form-container">
-        <Form.Group className='form__email'>
+        <Form.Group className='form__email localEmail domainEmail'>
           <Form.Label className='form__label'>이메일</Form.Label>
           <div className="form__email-input">
             <span className="email-input__local">
               <Form.Control type="email" placeholder="이메일"
                 onChange={e => setInputLocalEmail(e.target.value)}
-                onBlur={e => emptyErrorStyleController(inputLocalEmail, 'email', setIsEmailInputEmpty)}
+                onBlur={e => {
+                  emptyErrorHandler(inputLocalEmail, 'localEmail');
+                  errorStyleHandler(inputLocalEmail,'localEmail')
+                }}
               />  
             </span>
             <span className='email-input__separator'>@</span>
 
             <span className='email-input__domain'>
-              <Form.Select defaultValue='선택해주세요' 
-              onChange={e => setInputDomainEmail(e.target.value)}
-              onBlur={e => emptyErrorStyleController(inputDomainEmail,'email', setIsEmailInputEmpty)}
+              <Form.Select type="email" defaultValue='선택해주세요' 
+              onChange={e => {
+                setInputDomainEmail(e.target.value);
+                }}
+              onBlur={e => {
+                emptyErrorHandler(inputDomainEmail, 'domainEmail');
+                errorStyleHandler(inputDomainEmail,'domainEmail')}}
               >
                 <option value="선택해주세요" disabled>선택해주세요</option>
-                <option value="naver.com">naver.com</option>
+                <option value="naver.com">naver.com</option>  
                 <option value="daum.net">daum.net</option>
                 <option value="gmail.com">gmail.com</option>
                 <option value="직접 입력">직접 입력</option>
@@ -101,36 +145,54 @@ export default function SignUp({signUpEmail, jjongLogo2}){
             </span>
           </div>
           {
-            isEmailInputEmpty&& (
-            <div className='empty-error-div'>
+            (everyInputErrorList.getIn(['localEmail', 'emptyError']) || everyInputErrorList.getIn(['domainEmail', 'emptyError'])) && (
+            <div className='input-error-div'>
               필수 입력 항목입니다.
             </div>) 
-          }
+          }         
         </Form.Group>
       
-        <Form.Group className='form__password'>
+        <Form.Group className='form__password password'>
           <Form.Label className='form__label'>비밀번호</Form.Label>
           <p className="form__password-rule">6자 이상의 비밀번호를 입력해주세요.</p>
-          <Form.Control className="form__password-input" type="password" placeholder="비밀번호" 
-            onChange={e => setInputPassword(e.target.value)} 
-            onBlur={() => emptyErrorStyleController(inputPassword,'password', setIsPasswordInputEmpty)}/>
-          {
-            isPasswordInputEmpty && (
-            <div className='empty-error-div'>
-              필수 입력 항목입니다.
-            </div>) 
-          }
+            <Form.Control className="form__password-input" type="password" placeholder="비밀번호" 
+              onChange={e => setInputPassword(e.target.value)} 
+              onBlur={e => {
+                emptyErrorHandler(inputPassword, 'password');
+                errorStyleHandler(inputPassword, 'password')}} 
+            />
+            {
+              everyInputErrorList.getIn(['password', 'emptyError'])  && (
+              <div className='input-error-div'>
+                필수 입력 항목입니다.
+              </div>) 
+            }
+            {
+              everyInputErrorList.getIn(['password', 'shortError']) && (
+              <div className='input-error-div'>
+                비밀번호는 최소 6자리 이상이여야 합니다.
+              </div>) 
+            }
         </Form.Group>
 
-        <Form.Group className='form__password-check'>
+        <Form.Group className='form__password-check passwordCheck'>
           <Form.Label className='form__label'>비밀번호 확인</Form.Label>
           <Form.Control className="form__password-check-input" type="password" placeholder="비밀번호 확인" 
             onChange={e => setInputPasswordCheck(e.target.value)} 
-            onBlur={() => emptyErrorStyleController(inputPasswordCheck,'password-check', setIsPasswordCheckInputEmpty)}/>
+            onBlur={e => {
+              emptyErrorHandler(inputPasswordCheck, 'passwordCheck');
+              errorStyleHandler(inputPasswordCheck, 'passwordCheck')}} 
+            />
           {
-            isPasswordCheckInputEmpty && (
-            <div className='empty-error-div'>
+            everyInputErrorList.getIn(['passwordCheck', 'emptyError']) && (
+            <div className='input-error-div'>
               확인을 위해 비밀번호를 한 번 더 입력해주세요.
+            </div>) 
+          }
+          {
+            everyInputErrorList.getIn(['passwordCheck', 'notMatchError']) && (
+              <div className='input-error-div'>
+              비밀번호가 일치하지 않습니다.
             </div>) 
           }
         </Form.Group>
